8aac819f11a824115555eb09f987a83adc710ae7
akihiro.kawasaki <akihiro.kawasaki@rakuten.com>
2018-05-30 04:50:15 +0000

    CRUD tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>KadaiTasklist</title>
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+     <body>
+        @include('commons.error_messages')
+
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>task list</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->status }} > {{ $task->content }}</li>
+            @endforeach
+        </ul>
+        
+    @endif
+    
+{!! link_to_route('tasks.create', 'create page') !!}
+
+@endsection
