072fdc6702535ed09243db96ab128cfb4c897546
akihiro.kawasaki <akihiro.kawasaki@rakuten.com>
2018-06-01 00:46:35 +0000

    bootstrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,16 +1,19 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
-
-    <p>status: {{ $task->status }}</p>
-    <p>メッセージ: {{ $task->content }}</p>
-
-    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
-
-    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+  <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タイトル</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+      
+       {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
-
-@endsection
