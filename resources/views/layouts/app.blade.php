0efeb866baee2a80c4dab363defa8663f1f7fb82
akihiro.kawasaki <akihiro.kawasaki@rakuten.com>
2018-06-01 01:40:24 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <title>Kadai-tasklist</title>
 
         <!-- Bootstrap -->
+        
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -16,7 +17,7 @@
         @include('commons.navbar')
         
         <div class="container">
-            @include('commons.error_messages')
+            @include('commons.error_tasks')
             
             @yield('content')
         </div>
